a4b2a1afd9790f7ea588263285e2423d225485c1
YasmimVieira <vieiray937@gmail.com>
2022-09-08 21:33:20 -0300

    feature: add simple crud

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json())
+
 const livros = [
     {id: 1, "titulo": "Senhor dos aneis"},
     {id: 2, "titulo": "Senhor dos aneis 2"}
@@ -14,5 +16,39 @@ app.get('/', (req, res) => {
 app.get('/livros', (req, res) => {
     res.status(200).json(livros)
 });
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso");
+})
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivros(req.params.id);
+
+    livros[index].titulo = req.body
+    res.json(livros)
+})
+
+app.delete('/livros/:id', (req, res) => {
+    let { id } = req.params;
+    let index = buscaLivros(id);
+
+    livros.splice(index, 1);
+    res.send(`Livro ${id} excluido com sucesso`)
+})
+
+
+app.get('/livros/:id', (req, res) => {
+    let index = buscaLivros(req.params.id);
+
+    res.json(livros[index])
+})
+
+
+function buscaLivros(id) {
+    return livros.findIndex(livro => livro.id === id);
+}
+
+
 
 export default app; 
